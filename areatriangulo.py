0905358f5f6876c7006b795db5bacea7ac611c13
ana lorena camargo <ada_hack553@womenintech.mx>
2023-12-13 15:59:21 -0600

    first commit

---
+++ areatriangulo.py
@@ -0,0 +1,9 @@
+# Ask user for base and height of the triangle
+base = float(input("Enter the base of the triangle: "))
+height = float(input("Enter the height of the triangle: "))
+
+# Calculate the area of the triangle
+area = 0.5 * base * height
+
+# Display the result
+print(f"The area of the triangle with base is: {area}")
